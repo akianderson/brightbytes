4913fdce3abc7856f0d008ac4f5acd2d99c09b13
akianderson <s.aki.anderson@gmail.com>
2014-03-29 16:34:50 -0500

    Step2

--- calculator.rb
+++ calculator.rb
@@ -2,6 +2,7 @@ class Calculator
   def add(num_string)
     numbers = num_string.split(",").map(&:to_i)
     total = numbers.inject{|sum, x| sum += x}
+
     puts "#{numbers.join(' + ')} =  #{total}"
   end
 end
