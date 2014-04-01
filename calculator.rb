5d368b43b8e9e11eea23c5c13c91979b4025484c
akianderson <s.aki.anderson@gmail.com>
2014-03-31 22:02:52 -0500

    Step 3

--- calculator.rb
+++ calculator.rb
@@ -1,8 +1,22 @@
+require 'pry'
 class Calculator
   def add(num_string)
-    numbers = num_string.split(",").map(&:to_i)
-    total = numbers.inject{|sum, x| sum += x}
+    if check_input(num_string)
+      numbers = num_string.gsub("\n",",").split(",").map(&:to_i)
+      total = numbers.inject{|sum, x| sum += x}
 
-    puts "#{numbers.join(' + ')} =  #{total}"
+      puts "#{numbers.join(' + ')} =  #{total}"
+    else
+      puts "invalid input"
+    end
+  end
+
+  def check_input(num_string)
+    regex = Regexp.new(/[[[:digit:]]+[,][[:space:]]*]+/)
+    if num_string == regex.match(num_string).to_s
+      return true
+    else
+      return false
+    end
   end
 end
